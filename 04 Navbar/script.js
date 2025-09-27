12f8fe0191a121f8e1ef08cc584bb93685c19c99
jawwad9 <jawwadmuhammad514@gmail.com>
2025-09-27 12:41:24 -0700

    04 navbar complete

--- 04 Navbar/script.js
+++ 04 Navbar/script.js
@@ -1,8 +1,16 @@
-let icon = document.querySelector('.icon')
+let menu = document.querySelector('.icon')
 let ul = document.querySelector('ul')
+let bar = document.querySelector('#bar')
 
-icon.addEventListener('click', ()=>{
-    ul.classList.toggle('showData')
-    console.log('ul');
+
+
+menu.addEventListener("click", ()=>{
+    ul.classList.toggle("showData");
+    console.log(ul);
     
+    if(ul.classList == "showData"){
+        bar.classList= "fa-solid fa-xmark"
+    }else{
+        bar.classList= "fa-solid fa-list"
+    }
 })
